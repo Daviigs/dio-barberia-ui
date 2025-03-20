aa088420feb4087d2a7e1731d3cb656d19aca922
Davi Miquéias <miqueiasdavi8@gmail.com>
2025-03-19 22:53:40 -0300

    Criacao do Header e Menu de navegacao

--- src/app/commons/components/menu-bar/menu-ba.component.ts
+++ src/app/commons/components/menu-bar/menu-ba.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-ba',
-  imports: [],
+  imports: [MatMenuModule, MatButtonModule],
   templateUrl: './menu-ba.component.html',
   styleUrl: './menu-ba.component.scss'
 })
 export class MenuBaComponent {
 
+  constructor(private readonly router: Router) { }
+
+  navigateTo(path: string) {
+    this.router.navigate([path])
+  }
 }
