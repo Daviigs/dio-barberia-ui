163fd32a8fc793825d83972fe9c56d416ebf9bb7
Davi Miquéias <miqueiasdavi8@gmail.com>
2025-03-19 21:19:57 -0300

    Criando pagina de cadastro dos clientes

---
+++ src/app/clients/client.models.ts
@@ -0,0 +1,15 @@
+export interface ClientModelForm {
+
+    id?: number
+    name: string
+    email: string
+    phone: string
+}
+
+export interface ClientMOdelTable{
+
+    id: number
+    name: string
+    email: string
+    phone: string
+}
--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,19 +1,31 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { SERVICES_TOKEN } from '../../services/service.token';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { ClientFormComponent } from "../components/client-form/client-form.component";
+import { ClientModelForm } from '../client.models';
+
 
 @Component({
   selector: 'app-new-client',
-  imports: [],
+  imports: [ClientFormComponent],
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
   providers: [
   {provide: SERVICES_TOKEN.HTTP.CLIENTE, useClass: ClientsService}
   ]
 })
-export class NewClientComponent {
-
+export class NewClientComponent implements OnDestroy{
+ 
   constructor(@Inject (SERVICES_TOKEN.HTTP.CLIENTE)private readonly httpService: IClientService){}
 
+  ngOnDestroy() : void{
+    
+  }
+
+  onSubmitClient(value: ClientModelForm){
+    
+  }
+
 }
+
